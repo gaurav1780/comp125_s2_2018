5f7c3c23ac93582ede89d44cda62f32cd8641d09
Terry-Bui <terry.bui96@gmail.com>
2018-07-22 20:20:46 +1000

    more comments on the script that creates the navigation list yml file

--- makeNavigationList.py
+++ makeNavigationList.py
@@ -6,13 +6,13 @@ import frontmatter
 from pprint import pprint
 
 CONFIG = '_config.yml'
-OUTPUT = 'navigation-list.yml'
+DEST_FILE = 'navigation-list.yml'
 DATA_DIR = '_data'
 
 
 def dump(filepath, yaml, objyaml):
     """Dump the values from the dictionary of objyaml into the file specfiy
-    by filepath"""
+    by filepath."""
     def startendlines(s):
         """Prepend and append `---` around the str s."""
         return "---\n{0}---\n".format(s)
@@ -22,19 +22,19 @@ def dump(filepath, yaml, objyaml):
 
 def pretty_str(str):
     """Return the a version of str with '-' and '_' characters replaced
-    by ' ' (space) character and a titlecased version of str"""
+    by ' ' (space) character and a titlecased version of str."""
     new_str = str
     for ch in ['-', '_']:
         new_str = new_str.replace(ch, ' ')
     return new_str.title().strip()
 
 
-# Check if `_data` exist, else make `_data` directory
+# Check if `_data` exist, else make `_data` directory.
 if not path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
 
 # Open `_config.yml` file of the project and store the value of the front
-# matter     variable `collections_dir` into the COLLECTIONS_DIR_NAME
+# matter variable `collections_dir` into the COLLECTIONS_DIR_NAME.
 with open(CONFIG) as config_file:
     CONFIG_YAML = YAML().load(config_file.read())
     COLLECTIONS_DIR_NAME = CONFIG_YAML['collections_dir']
@@ -42,6 +42,9 @@ with open(CONFIG) as config_file:
 ROOT = os.getcwd()
 COLLECTION_DIR_FULL_PATH = ROOT + '/' + COLLECTIONS_DIR_NAME
 
+# List of collection names that begin with a digit, because all
+# collection directory must be prepended with a '_' (underscore)
+# character, we check if the 2nd character is a digit
 DIR_LIST = [
     item for item in sorted(
         os.listdir(COLLECTIONS_DIR_NAME)) if os.path.isdir(
@@ -49,8 +52,14 @@ DIR_LIST = [
                 COLLECTIONS_DIR_NAME,
                 item)) and item[1].isdigit()]
 
+# Name of the front matter variable
 FRONT_MATTER_LIST_NAME = 'navigation_list'
+
+# The dictionary that will store the front matter
 NAV_LIST = {FRONT_MATTER_LIST_NAME: []}
+
+# For each directory from DIR_LIST, create a list that stores
+# the title of the file and the link of the file.
 for dir_name in DIR_LIST:
     pretty_dir_name = pretty_str(dir_name[3:])
     NAV_LIST[FRONT_MATTER_LIST_NAME].append(
@@ -60,9 +69,16 @@ for dir_name in DIR_LIST:
         filename = os.fsdecode(file)
         title = ''
         page_path = full_dir_name + '/' + filename
+        # Link is the filename excluding the file extension
         link = filename.split(".")[0]
         with open(page_path) as page_file:
             PAGE_YAML, content = frontmatter.parse(page_file.read())
+            # If file have 'title' front matter variable then that will
+            # be the value in navigation-list.yml for that file,
+            # otherwise the title will be version of the link with the
+            # prepend 'xx-' removed, where 'xx' is a digit and the
+            # word is titlecased and all '-' character in the string
+            # is replaced with the ' ' character.
             if 'title' in PAGE_YAML:
                 title = PAGE_YAML['title']
             else:
@@ -70,5 +86,8 @@ for dir_name in DIR_LIST:
         NAV_LIST[FRONT_MATTER_LIST_NAME][-1]['links'].append(
             {'title': title, 'link': link})
 
-dump(DATA_DIR + '/' + OUTPUT, YAML(), NAV_LIST)
+# Write the content of NAV_LIST
+FULL_OUTPUT_PATH = DATA_DIR + '/' + DEST_FILE
+dump(FULL_OUTPUT_PATH, YAML(), NAV_LIST)
+
 pprint(NAV_LIST)
