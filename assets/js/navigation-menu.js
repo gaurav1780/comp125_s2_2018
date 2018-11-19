3d70457e7e135174f559135107ba2a4bec34e655
Terry-Bui <terry.bui96@gmail.com>
2018-11-19 14:54:36 +1100

    onClick a collection on Lecture Topics, open up navigation menu for that collection

--- assets/js/navigation-menu.js
+++ assets/js/navigation-menu.js
@@ -1,7 +1,15 @@
+---
+layout: null
+---
 'use strict';
 
-$(document).ready(function(){
-  $('#01-introduction').click(function(e) {
+var introduction = '01-introduction';
+
+function toggleNavBar(collectionName) {
+  var collectionId = '#' + collectionName;
+  var collectionNavbar = collectionId + '-navbar';
+  console.log(collectionNavbar);
+  $(collectionId).click(function(e) {
     // Kill click event, otherwise Bootstrap will treat it as a click outside of the dropdown and immediately close it.
     // Source: https://stackoverflow.com/questions/22842903/how-to-open-bootstrap-dropdown-programmatically
     e.stopPropagation();
@@ -19,21 +27,28 @@ $(document).ready(function(){
     
     // Toggle ul
     
-    $('#01-introduction-navbar').css('display', 'block');
-    $('#01-introduction-navbar').css('top', '0%');
-    $('#01-introduction-navbar').css('left', '100%');
-    $('#01-introduction-navbar').css('position', 'absolute');
+    $(collectionNavbar).css('display', 'block');
+    $(collectionNavbar).css('top', '0%');
+    $(collectionNavbar).css('left', '100%');
+    $(collectionNavbar).css('position', 'absolute');
     // Set timeout so it revertws back to hidden.
-    //
     function removeStyling() {
-      $('#01-introduction-navbar').css('display', '');
-      $('#01-introduction-navbar').css('top', '');
-      $('#01-introduction-navbar').css('left', '');
-      $('#01-introduction-navbar').css('position', '');
+      $(collectionNavbar).css('display', '');
+      $(collectionNavbar).css('top', '');
+      $(collectionNavbar).css('left', '');
+      $(collectionNavbar).css('position', '');
 
     }
     var duration = 5000; //In miliseconds
     setTimeout(removeStyling, duration);
 
   });
+}
+
+$(document).ready(function(){
+  //toggleNavBar(introduction);
+  {% for collection in site.data.navigation-list.navigation_list %}
+    {%- assign id = collection.id -%}
+    toggleNavBar('{{ id }}');
+  {% endfor %}
 });
